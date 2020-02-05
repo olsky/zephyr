1ebf349586d8877820c1662595f654316dcfbf17
bwasim <bilal_wasim@mentor.com>
2020-02-05 21:17:41 +0500

    Base commit of the ublox c030-u201 board. Builds with warnings.. Still to be tested..

---
+++ boards/arm/ublox_c030_u201/pinmux.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2017 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <kernel.h>
+#include <device.h>
+#include <init.h>
+#include <drivers/pinmux.h>
+#include <sys/sys_io.h>
+
+static int pinmux_stm32_init(struct device *port)
+{
+	/* Empty stub for now. Will add code later. */
+	return 0;
+}
+
+SYS_INIT(pinmux_stm32_init, PRE_KERNEL_1,
+	 CONFIG_PINMUX_STM32_DEVICE_INITIALIZATION_PRIORITY);
