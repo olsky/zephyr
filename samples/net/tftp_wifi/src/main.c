6ede3d78ff54e85e3ab6c704289aecae3d879eee
Bilal Wasim <bilal_wasim@mentor.com>
2020-02-09 17:42:05 +0500

    Cleanup of the application..

--- samples/net/tftp_wifi/src/main.c
+++ samples/net/tftp_wifi/src/main.c
@@ -14,11 +14,24 @@
 #include <net/wifi_mgmt.h>
 #include <net/net_event.h>
 
+/* Support for TFTP. */
 #include <net/tftp.h>
 
-/* Hardcoded SSID / Password for now. Will update soon to get it in a better way. */
+/* Hardcoded SSID / Password for now. Should be updated by the user per their config. */
 #define MY_SSID     "GalaxyJ7"
 #define PASS        "bilalwasim"
+
+/* Hardcoded values for Server IP / Port. Should be updated by the user per their config. */
+#define TFTPS_IP     "192.168.43.138"
+#define TFTPS_PORT   51000
+
+/* User buffer and structure for getting / putting data from / to the TFTP Server. */
+static u8_t  buf[16384];
+struct tftpc tmp_struct = {
+	.user_buf      = (u8_t *) buf, /* Pointer to user buffer. */
+	.user_buf_size = 16384         /* Size of the buffer. */
+};
+
 
 /* Function: WiFi_Setup
  * Description: This function tries to connect with a given SSID / Password pair.  */
@@ -36,10 +49,6 @@ static int32_t WiFi_Setup(void)
 	cnx_params.channel     = WIFI_CHANNEL_ANY;
 	cnx_params.security    = WIFI_SECURITY_TYPE_PSK;
 
-	/* Log(s). */
-//	LOG_INF("Starting WiFi thread\r\n");
-//	LOG_INF("Sending request to connect with SSID: %s, Password: %s\r\n", MY_SSID, PASS);
-
 	/* Connect with the default "GalaxyJ7" SSID. */
 	net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &cnx_params, sizeof(struct wifi_connect_req_params));
 
@@ -47,26 +56,18 @@ static int32_t WiFi_Setup(void)
 	return (net_mgmt_event_wait(NET_EVENT_WIFI_CONNECT_RESULT, NULL, &iface, NULL, NULL, K_FOREVER));
 }
 
-static u8_t buf[16384];
-
-struct tftpc tmp_struct = {
-	.user_buf = (u8_t *) buf,
-	.user_buf_size = 16384
-};
-
 void main(void)
 {
 	int                ret;
 	struct sockaddr_in server;
 
-	/* Fill in the server info. */
-	/* 		addr4.sin_family = AF_INET;
-		addr4.sin_port = htons(PEER_PORT);
-		inet_pton(AF_INET, CONFIG_NET_CONFIG_PEER_IPV4_ADDR,
-			  &addr4.sin_addr); */
+	/* Fill in the server info.
+	 * - Use INET.
+	 * - Use UDP Port  - TFTPS_PORT.
+	 * - Use Server IP - TFTPS_IP. */
 	server.sin_family = AF_INET;
-	server.sin_port = htons(51000);
-	inet_pton(AF_INET, "192.168.43.138", &server.sin_addr);
+	server.sin_port   = htons(TFTPS_PORT);
+	inet_pton(AF_INET, TFTPS_IP, &server.sin_addr);
 
 	/* Log message. */
 	printk("Hello World! %s\n", CONFIG_BOARD);
