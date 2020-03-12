8b0439dde78f3f1f5f36b876e93e78f32859fef5
Bilal Wasim <bilal_wasim@mentor.com>
2020-03-13 04:34:17 +0500

    Getting the nRF9160_innblue22 building with the Zephyr Master Branch (still some warnings). Here's the list of changes, - ARM / SOC_FAMILY_NRF are not directly user-configurable anymore and are selected automatically based on the SoC series. Therefore these enables have been removed from the defconfig. - Including proper header files in the board_init.c file and doing tabs->space.

--- boards/arm/nrf9160_innblue22/innblue22_board_init.c
+++ boards/arm/nrf9160_innblue22/innblue22_board_init.c
@@ -6,35 +6,34 @@
  */
 
 #include <init.h>
-#include <gpio.h>
-#include <misc/printk.h>
+#include <drivers/gpio.h>
 
 // always on: #define VDD_3V3_PWR_CTRL_GPIO_PIN  12   // ENABLE_3V3_SENSOR --> i2c sensors
 #define VDD_5V0_PWR_CTRL_GPIO_PIN  21   // ENABLE_5V0_BOOST  --> speed sensor
 
 struct pwr_ctrl_cfg {
-	const char *port;
-	u32_t pin;
+    const char *port;
+    u32_t pin;
 };
 
 static int pwr_ctrl_init(struct device *dev)
 {
-	const struct pwr_ctrl_cfg *cfg = dev->config->config_info;
-	struct device *gpio;
-	printk("innblue > %s [%d] > bind device \"%s\" pin %d\n", __func__, __LINE__, cfg->port, cfg->pin);
+    const struct pwr_ctrl_cfg *cfg = dev->config->config_info;
+    struct device *gpio;
+    printk("innblue > %s [%d] > bind device \"%s\" pin %d\n", __func__, __LINE__, cfg->port, cfg->pin);
 
-	gpio = device_get_binding(cfg->port);
-	if (!gpio) {
-		printk("innblue > %s [%d] > Could not bind device \"%s\"\n", __func__, __LINE__, cfg->port);
-		return -ENODEV;
-	}
+    gpio = device_get_binding(cfg->port);
+    if (!gpio) {
+        printk("innblue > %s [%d] > Could not bind device \"%s\"\n", __func__, __LINE__, cfg->port);
+        return -ENODEV;
+    }
 
-	gpio_pin_configure(gpio, cfg->pin, GPIO_DIR_OUT);
-	gpio_pin_write(gpio, cfg->pin, 1);
+    gpio_pin_configure(gpio, cfg->pin, GPIO_DIR_OUT);
+    gpio_pin_write(gpio, cfg->pin, 1);
 
-	k_sleep(1); /* Wait for the rail to come up and stabilize */
+    k_sleep(1); /* Wait for the rail to come up and stabilize */
 
-	return 0;
+    return 0;
 }
 
 /*
@@ -48,29 +47,29 @@ static int pwr_ctrl_init(struct device *dev)
 /* ??? 
 #if CONFIG_BOARD_VDD_PWR_CTRL_INIT_PRIORITY <= CONFIG_GPIO_NRF_INIT_PRIORITY
 #error GPIO_NRF_INIT_PRIORITY must be lower than \
-	BOARD_VDD_PWR_CTRL_INIT_PRIORITY
+    BOARD_VDD_PWR_CTRL_INIT_PRIORITY
 #endif
 */
 
 
 /*
 static const struct pwr_ctrl_cfg vdd_3v3_pwr_ctrl_cfg = {
-	.port = DT_GPIO_P0_DEV_NAME,
-	.pin  = VDD_3V3_PWR_CTRL_GPIO_PIN,
+    .port = DT_GPIO_P0_DEV_NAME,
+    .pin  = VDD_3V3_PWR_CTRL_GPIO_PIN,
 };
 
 DEVICE_INIT(vdd_3v3_pwr_ctrl_init, "", pwr_ctrl_init, NULL, &vdd_3v3_pwr_ctrl_cfg,
-	    POST_KERNEL, 70);
+        POST_KERNEL, 70);
 */
-		
-		
+        
+        
 static const struct pwr_ctrl_cfg vdd_5v0_pwr_ctrl_cfg = {
-	.port = DT_GPIO_P0_DEV_NAME,
-	.pin  = VDD_5V0_PWR_CTRL_GPIO_PIN,
+    .port = DT_GPIO_P0_DEV_NAME,
+    .pin  = VDD_5V0_PWR_CTRL_GPIO_PIN,
 };
 
 DEVICE_INIT(vdd_5v0_pwr_ctrl_init, "", pwr_ctrl_init, NULL, &vdd_5v0_pwr_ctrl_cfg,
-	    POST_KERNEL, 70);
+        POST_KERNEL, 70);
 
-		
+        
 
