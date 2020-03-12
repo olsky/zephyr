ba2d81755f901fc71f66031f7fa9e0a6214ee57f
Bilal Wasim <bilal_wasim@mentor.com>
2020-03-13 04:52:29 +0500

    Getting rid of the redundant structure and using the mappings directly.. Has no side effects..

--- boards/arm/nrf9160_innblue22/innblue22_board_init.c
+++ boards/arm/nrf9160_innblue22/innblue22_board_init.c
@@ -9,38 +9,28 @@
 
 #define VDD_5V0_PWR_CTRL_GPIO_PIN  21   // ENABLE_5V0_BOOST  --> speed sensor
 
-struct pwr_ctrl_cfg {
-    const char *port;
-    u32_t pin;
-};
-
 static int pwr_ctrl_init(struct device *dev)
 {
     const struct pwr_ctrl_cfg *cfg = dev->config->config_info;
     struct device *gpio;
-    printk("innblue > %s [%d] > bind device \"%s\" pin %d\n", __func__, __LINE__, cfg->port, cfg->pin);
+    printk("innblue > %s [%d] > bind device \"%s\" pin %d\n", __func__, __LINE__, DT_GPIO_P0_DEV_NAME, VDD_5V0_PWR_CTRL_GPIO_PIN);
 
-    gpio = device_get_binding(cfg->port);
+    gpio = device_get_binding(DT_GPIO_P0_DEV_NAME);
     if (!gpio) {
-        printk("innblue > %s [%d] > Could not bind device \"%s\"\n", __func__, __LINE__, cfg->port);
+        printk("innblue > %s [%d] > Could not bind device \"%s\"\n", __func__, __LINE__, DT_GPIO_P0_DEV_NAME);
         return -ENODEV;
     }
 
-    gpio_pin_configure(gpio, cfg->pin, GPIO_DIR_OUT);
-    gpio_pin_write(gpio, cfg->pin, 1);
+    gpio_pin_configure(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN, GPIO_DIR_OUT);
+    gpio_pin_write(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN, 1);
 
     k_sleep(1); /* Wait for the rail to come up and stabilize */
 
     return 0;
 }
 
-static const struct pwr_ctrl_cfg vdd_5v0_pwr_ctrl_cfg = {
-    .port = DT_GPIO_P0_DEV_NAME,
-    .pin  = VDD_5V0_PWR_CTRL_GPIO_PIN,
-};
-
-DEVICE_INIT(vdd_5v0_pwr_ctrl_init, "", pwr_ctrl_init, NULL, &vdd_5v0_pwr_ctrl_cfg,
-        POST_KERNEL, 70);
+DEVICE_INIT(vdd_5v0_pwr_ctrl_init, "", pwr_ctrl_init, NULL, NULL,
+            POST_KERNEL, 70);
 
         
 
