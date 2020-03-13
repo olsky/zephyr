504ee2f2a8a83914c261adb93283687377319f83
Bilal Wasim <bilal_wasim@mentor.com>
2020-03-13 05:00:29 +0500

    Fixing up the warnings in the power control function..

--- boards/arm/nrf9160_innblue22/innblue22_board_init.c
+++ boards/arm/nrf9160_innblue22/innblue22_board_init.c
@@ -7,26 +7,32 @@
 #include <init.h>
 #include <drivers/gpio.h>
 
-#define VDD_5V0_PWR_CTRL_GPIO_PIN  21   // ENABLE_5V0_BOOST  --> speed sensor
+#define VDD_5V0_PWR_CTRL_GPIO_PIN  21   /* ENABLE_5V0_BOOST  --> speed sensor */
 
 static int pwr_ctrl_init(struct device *dev)
 {
-    const struct pwr_ctrl_cfg *cfg = dev->config->config_info;
     struct device *gpio;
-    printk("innblue > %s [%d] > bind device \"%s\" pin %d\n", __func__, __LINE__, DT_GPIO_P0_DEV_NAME, VDD_5V0_PWR_CTRL_GPIO_PIN);
+    int    ret = -ENODEV;
 
+    /* Get handle of the GPIO device. */
     gpio = device_get_binding(DT_GPIO_P0_DEV_NAME);
-    if (!gpio) {
-        printk("innblue > %s [%d] > Could not bind device \"%s\"\n", __func__, __LINE__, DT_GPIO_P0_DEV_NAME);
-        return -ENODEV;
+
+    /* Valid handle? */
+    if (gpio != NULL) {
+
+        /* Configure this pin as output. */
+        if ((ret = gpio_pin_configure(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN, GPIO_OUTPUT_ACTIVE)) == 0) {
+
+            /* Write "1" to this pin. */
+            ret = gpio_pin_set(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN, 1);
+        }
+
+        /* Wait for the rail to come up and stabilize. */
+        k_sleep(1);
     }
 
-    gpio_pin_configure(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN, GPIO_DIR_OUT);
-    gpio_pin_write(gpio, VDD_5V0_PWR_CTRL_GPIO_PIN, 1);
-
-    k_sleep(1); /* Wait for the rail to come up and stabilize */
-
-    return 0;
+    /* Operation status? */
+    return (ret);
 }
 
 DEVICE_INIT(vdd_5v0_pwr_ctrl_init, "", pwr_ctrl_init, NULL, NULL,
